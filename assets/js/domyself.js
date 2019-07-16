c819169fd5aed02c95075496bc095f1d60aaee2e
ryansims265 <ryansims265@gmail.com>
2019-07-15 20:39:00 -0400

    Got Youtube API comfigured and video displays in iframe

--- assets/js/domyself.js
+++ assets/js/domyself.js
@@ -1,31 +1,19 @@
-// keyword = "mechanic"
 
-//     var queryURL = "https://www.googleapis.com/youtube/v3/search=" + keyword + "&key=AIzaSyAgA2B82VMDKMe0skNBKgiM0fIOTqBZPG0";
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
 
-//       // Printing the entire object to console
-//       console.log(response);
-//     });
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=oil_change&key=AIzaSyAgA2B82VMDKMe0skNBKgiM0fIOTqBZPG0"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      // Printing the entire object to console
+      console.log(response.items[1].id.videoId);
+      var videoid = response.items[1].id.videoId;
+      document.getElementById("video-here").src = "https://www.youtube.com/embed/" + videoid;
+
+    });
   
 
-// // Load the IFrame Player API code asynchronously.
-// var tag = document.createElement('script');
-// tag.src = "https://www.youtube.com/player_api";
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// // Replace the 'ytplayer' element with an <iframe> and
-// // YouTube player after the API code downloads.
-// var player;
-// function onYouTubePlayerAPIReady() {
-//   player = new YT.Player('ytplayer', {
-//     height: '360',
-//     width: '640',
-//     videoId: 'M7lc1UVf-VE'
-//   });
-// }
-
-
+  
