59ad41a02a7092b517b68b042a34f66a7afcc743
Froglegg <hayescrowley@gmail.com>
2019-07-23 19:39:13 -0400

    yep

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-$("#create-user").on("click", function (event) {
+$("#create-user").on("click", function(event) {
     event.preventDefault();
     // Get inputs
     email = $("#inputEmail").val().trim();
@@ -31,6 +31,7 @@ $("#create-user").on("click", function (event) {
         oilchange: oilchange,
         mileage: mileage,
     });
+
 });
 
 //This hides the user signup form until they click the register button 
@@ -60,53 +61,50 @@ function showForm() {
 
 var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=oil_change&key=AIzaSyAgA2B82VMDKMe0skNBKgiM0fIOTqBZPG0"
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
 
-      // Printing the entire object to console
-      console.log(response.items[1].id.videoId);
-      var videoid = response.items[1].id.videoId;
-      document.getElementById("video-here").src = "https://www.youtube.com/embed/" + videoid;
+    // Printing the entire object to console
+    console.log(response.items[1].id.videoId);
+    var videoid = response.items[1].id.videoId;
+    document.getElementById("video-here").src = "https://www.youtube.com/embed/" + videoid;
 
-    });
-
-
-
-    var proxyurl = "https://cors-anywhere.herokuapp.com/";
-    var mapsurl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.786,-84.379&radius=5000&fields=name,formatted_address,rating&type=car_repair&keyword=oil&key=AIzaSyDg7arbjgsAKEij1dEAJONeKoNFX005rbs"; 
-    
-    $.ajax({
-      url: proxyurl + mapsurl,
-      method: "GET"
-    }).then(function(response) {
-      // Printing the entire object to console
-      console.log(response.results[2]);
-      var address1 = response.results[2].vicinity;
-      var name1 = response.results[2].name;
-      var rating1 = response.results[2].rating;
-    
-      $("#serviceName1").html(name1);
-      $("#serviceAddress1").html(address1);
-      $("#serviceRating1").html(rating1);
-    
-      var address2 = response.results[3].vicinity;
-      var name2 = response.results[3].name;
-      var rating2 = response.results[3].rating;
-    
-      $("#serviceName2").html(name2);
-      $("#serviceAddress2").html(address2);
-      $("#serviceRating2").html(rating2);
-
-      var address3 = response.results[1].vicinity;
-      var name3 = response.results[1].name;
-      var rating3 = response.results[1].rating;
-    
-      $("#serviceName3").html(name3);
-      $("#serviceAddress3").html(address3);
-      $("#serviceRating3").html(rating3);
-    });
+});
 
 
-    
+
+var proxyurl = "https://cors-anywhere.herokuapp.com/";
+var mapsurl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.786,-84.379&radius=5000&fields=name,formatted_address,rating&type=car_repair&keyword=oil&key=AIzaSyDg7arbjgsAKEij1dEAJONeKoNFX005rbs";
+
+$.ajax({
+    url: proxyurl + mapsurl,
+    method: "GET"
+}).then(function(response) {
+    // Printing the entire object to console
+    console.log(response.results[2]);
+    var address1 = response.results[2].vicinity;
+    var name1 = response.results[2].name;
+    var rating1 = response.results[2].rating;
+
+    $("#serviceName1").html(name1);
+    $("#serviceAddress1").html(address1);
+    $("#serviceRating1").html(rating1);
+
+    var address2 = response.results[3].vicinity;
+    var name2 = response.results[3].name;
+    var rating2 = response.results[3].rating;
+
+    $("#serviceName2").html(name2);
+    $("#serviceAddress2").html(address2);
+    $("#serviceRating2").html(rating2);
+
+    var address3 = response.results[1].vicinity;
+    var name3 = response.results[1].name;
+    var rating3 = response.results[1].rating;
+
+    $("#serviceName3").html(name3);
+    $("#serviceAddress3").html(address3);
+    $("#serviceRating3").html(rating3);
+});
