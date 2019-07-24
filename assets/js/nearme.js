559d8816e3f8b1b89c62756279e19f22168fa0d1
Ryan Sims <44000703+ryansims265@users.noreply.github.com>
2019-07-23 21:26:50 -0400

    Update nearme.js

--- assets/js/nearme.js
+++ assets/js/nearme.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    var uluru = { lat: 33.786, lng: -84.379 };
+    var uluru = {lat: 33.786, lng: -84.379};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: uluru });
+        document.getElementById('map'), {zoom: 4, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({ position: uluru, map: map });
 }
